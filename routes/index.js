3044c85df57ee46caefc4d6252704f6121374aef
Judit Calero <judit.calero84@gmail.com>
2024-02-01 13:15:58 +0100

    all connected

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
+const db = require("../model/helper");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.send({ title: 'Express' });
+});
+
+router.get('/exercicis/', function(req, res, next) { 
+
+  // the URL query comes in the shape of a string with each chord/scale/interval separated by a comma,
+  // but we want a string with each chord/scale/interval between "" and separated by a comma,
+  // so I grab the string, SPLIT it into an array, MAP through the array to give "" to each chord/scale/interval,
+  // then join it back with "chord/scale/interval1","chord/scale/interval2" sintax that mysql can read ^^
+  db(`SELECT * FROM exercicis`)
+  .then(results => {
+    res.send(results.data);
+  })
+   .catch(err => res.status(500).send(err));
 });
 
 module.exports = router;
