5289a3a64ccb48d7dfbf947002c08547110b6918
Judit Calero <judit.calero84@gmail.com>
2024-02-07 18:50:05 +0100

    played with computed refs and messed a bit with style

--- client/src/stores/counter.js
+++ client/src/stores/counter.js
@@ -2,11 +2,15 @@ import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
 
 export const useCounterStore = defineStore('counter', () => {
-  const count = ref(0)
+  const count = ref(10)
   const doubleCount = computed(() => count.value * 2)
   function increment() {
     count.value++
   }
 
-  return { count, doubleCount, increment }
+  function clear(){
+    count.value = 0;
+  }
+
+  return { count, doubleCount, increment, clear }
 })
--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,7 +1,9 @@
 <script setup>
-import { ref, onMounted } from 'vue';
+import { ref, onMounted, computed } from 'vue';
 import TestGrid from '../components/TestGrid.vue'
+import { useCounterStore } from '@/stores/counter';
 
+const counterData = useCounterStore();
 
 const count = ref(20)
 const aStringToMessWith = ref("we're here, we're queer, visca el Frankfurt Vallès.")
@@ -9,6 +11,11 @@ const exercicis = ref([])
 const gridCols = ref([])
 const isRed = ref(false)
 const isMounted = ref(false)
+
+
+const exercicisNotEmpty = computed(() => {
+  return exercicis.value.length ? "there are exercicis." : "there are no exercicis."
+})
 
 function increase(){
   count.value++
@@ -65,31 +72,36 @@ onMounted(() => {
 <template>
   <main>
     <div class="test_app_body">
-    <v-btn @click="() => increase()">Increase the count</v-btn>
+      <div class="grid_list">
+        <v-btn class="button" @click="() => increase()">Increase the count</v-btn>
+        <v-btn class="button" @click="() => clearCount()">Clear</v-btn>
+        <v-btn class="button" @click="() => reverseString()">Reverse the string.</v-btn>
+        <v-btn class="button" :style='{backgroundColor: isRed ? "white" : "red"}' @click="() => switchToRed()">change text color to {{ isRed ? "white" : "red" }}.</v-btn>
+        <v-btn class="button" @click="() => counterData.increment()">Increase the STORE count</v-btn>
+        <v-btn class="button" @click="() => counterData.clear()">Clear the STORE count</v-btn>
 
-    <v-btn class="button" @click="() => clearCount()">Clear</v-btn>
-    <v-btn class="button" @click="() => reverseString()">Reverse the string.</v-btn>
-    <v-btn class="button" :style='{backgroundColor: isRed ? "white" : "red"}' @click="() => switchToRed()">change the color of the text to {{ isRed ? "white" : "red" }}.</v-btn>
-    <p>Count is: {{ count }}</p>
-    <p>a <b>BOLD</b> statement: {{ aStringToMessWith }}</p>
+      </div>
 
-    <p :class="{ red: isRed }">a text to switch the color of.</p>
-    <div class="grid_list">
-      <!-- <v-card variant="elevated" color="indigo" elevation="16" hover v-for="(exercici, index) in exercicis">
-        
-        <v-card-title>{{ exercici.id }}</v-card-title>
-        <v-card-subtitle>Tipus d'exercici: {{ exercici.tipus }}</v-card-subtitle>
-        <v-card-subtitle>Espècie: {{ exercici.especie }}</v-card-subtitle>
-        
-      </v-card> -->
 
+      <p>Count is: {{ count }}</p>
+      <p>a <b>BOLD</b> statement: {{ aStringToMessWith }}</p>
+
+      <p :class="{ red: isRed }">a text to switch the color of.</p>
     </div>
-  </div>
 
-  <TestGrid
-    :data="exercicis"
-    :columns="gridCols">
-  </TestGrid>
+    {{ exercicisNotEmpty }} <br>
+    PINIA store count: {{ counterData.count }} <br>
+    PINIA store double count: {{ counterData.doubleCount }}
+
+    <div>
+      <TestGrid style="margin: auto;"
+        :data="exercicis"
+        :columns="gridCols">
+      </TestGrid>
+    </div>
+  
+
+ 
 
   </main>
 </template>
@@ -98,9 +110,9 @@ onMounted(() => {
 
 .grid_list{
   display: grid;
-  grid-template-columns: repeat(5, 1fr);
+  grid-template-columns: repeat(3, 1fr);
   grid-gap: 10px;
-  grid-auto-rows: minmax(100px, auto);
+  grid-auto-rows: minmax(10px, auto);
 }
 
 .card {
